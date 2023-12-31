2219f172e061dc4b53940a757cd657b2a7e40bb2
Nestor162 <nestorcicardini@gmail.com>
2023-12-31 13:47:21 +0100

    Draw the scaffold and the hanged man

--- hangman.py
+++ hangman.py
@@ -27,6 +27,14 @@ class Hangman:
             finalize=True,
         )
 
+        self._canvas = self.window["-CANVAS-"]
+
+        # Temporary code
+        self._draw_scaffold()
+        for index in range(MAX_WRONG_GUESSES):
+            self._wrong_guesses = index + 1
+            self._draw_hanged_man()
+
     def read_event(self):
         event = self.window.read()
         event_id = event[0] if event is not None else None
@@ -56,6 +64,60 @@ class Hangman:
             ],
             font="Any 20",
         )
+
+    def _draw_scaffold(self):
+        lines = [
+            ((40, 55), (180, 55), 10),
+            ((165, 60), (165, 365), 10),
+            ((160, 360), (100, 360), 10),
+            ((100, 365), (100, 330), 10),
+            ((100, 330), (100, 310), 1),
+        ]
+        for *points, width in lines:
+            self._canvas.DrawLine(*points, color="black", width=width)
+
+    def _draw_hanged_man(self):
+        head = (100, 290)
+        torso = [((100, 270), (100, 170))]
+        left_arm = [
+            ((100, 250), (80, 250)),
+            ((80, 250), (60, 210)),
+            ((60, 210), (60, 190)),
+        ]
+        right_arm = [
+            ((100, 250), (120, 250)),
+            ((120, 250), (140, 210)),
+            ((140, 210), (140, 190)),
+        ]
+        left_leg = [
+            ((100, 170), (80, 170)),
+            ((80, 170), (70, 140)),
+            ((70, 140), (70, 80)),
+            ((70, 80), (60, 80)),
+        ]
+        right_leg = [
+            ((100, 170), (120, 170)),
+            ((120, 170), (130, 140)),
+            ((130, 140), (130, 80)),
+            ((130, 80), (140, 80)),
+        ]
+        body = [
+            torso,
+            left_arm,
+            right_arm,
+            left_leg,
+            right_leg,
+        ]
+        if self._wrong_guesses == 1:
+            self._canvas.DrawCircle(
+                head,
+                20,
+                line_color="red",
+                line_width=2,
+            )
+        elif self._wrong_guesses > 1:
+            for part in body[self._wrong_guesses - 2]:
+                self._canvas.DrawLine(*part, color="red", width=2)
 
     def _build_letters_frame(self):
         letter_groups = [
